235aa236f42d5d756d2341ecc63241e69cf62882
Anders Ingemann <anders@ingemann.de>
2014-07-02 22:45:31 +0200

    Make help menu POSIX compliant

--- lib/commands/help.sh
+++ lib/commands/help.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
 function help {
-	if [[ $1 ]]; then
-		extended_help $1
-		exit $EX_SUCCESS
-	fi
+  if [[ $1 ]]; then
+    extended_help $1
+    exit $EX_SUCCESS
+  fi
 printf "homes${bldblu}h${txtdef}ick uses git in concert with symlinks to track your precious dotfiles.
 
- Usage: homeshick [options] TASK
+Usage:
+  homeshick cd <castle>
+  homeshick clone [-fhq] <uri>...
+  homeshick generate [-hq] <castle>...
+  homeshick list [-h]
+  homeshick check [-hq] [<castle>...]
+  homeshick refresh [-bhq] [--days=<days>] [<castle>...]
+  homeshick pull [-bhq] [<castle>...]
+  homeshick link [(-f|-s)] [-bhq] [<castle>...]
+  homeshick track [-fbhq] <castle> <file>...
+  homeshick -h | --help
 
- Tasks:
-  homeshick cd CASTLE                 # Enter a castle
-  homeshick clone URI..               # Clone URI as a castle for homeshick
-  homeshick generate CASTLE..         # Generate a castle repo
-  homeshick list                      # List cloned castles
-  homeshick check [CASTLE..]          # Check a castle for updates
-  homeshick refresh [DAYS] [CASTLE..] # Check if a castle needs refreshing
-  homeshick pull [CASTLE..]           # Update a castle
-  homeshick link [CASTLE..]           # Symlinks all dotfiles from a castle
-  homeshick track CASTLE FILE..       # Add a file to a castle
-  homeshick help [TASK]               # Show usage of a task
+Options:
+  -f --overwrite   Overwrite files that already exist
+  -s --skip        Skip files that already exist
+  -b --batch       Batch-mode: Skip interactive prompts / Choose the default
+  -h --help        Show description and usage of invoked command
+  -q --quiet       Suppress status output
+  -d --days        Refresh threshold [default: 7]
 
- Aliases:
-  symlink # Alias to link
-  updates # Alias to check
-
- Runtime options:
-   -q, [--quiet]    # Suppress status output
-   -s, [--skip]     # Skip files that already exist
-   -f, [--force]    # Overwrite files that already exist
-   -b, [--batch]    # Batch-mode: Skip interactive prompts / Choose the default
-
- Note:
+Note:
   To check, refresh, pull or symlink all your castles
   simply omit the CASTLE argument
-
 "
 }
 
@@ -45,47 +40,43 @@ function help_err {
 function extended_help {
 	case $1 in
 		cd)
-      printf "Enters a castle's home directory.\n"
-      printf "NOTE: For this to work, homeshick must be invoked via homeshick.{sh,csh,fish}.\n\n"
-      printf "Usage:\n  homeshick cd CASTLE"
+      printf "Enters a castle's directory.\n\n"
+      printf "Usage:\n  homeshick cd <castle>\n\n"
+      printf "Note:\n  For this to work, homeshick must be invoked via homeshick.{sh,csh,fish}.\n"
       ;;
 		clone)
-      printf "Clones URI as a castle for homeshick\n"
-      printf "Usage:\n  homeshick clone URL.."
+      printf "Clones URI as a castle for homeshick\n\n"
+      printf "Usage:\n  homeshick clone [-fhq] <uri>...\n"
       ;;
 		generate)
-      printf "Generates a repo prepped for usage with homeshick\n"
-      printf "Usage:\n  homeshick generate CASTLE.."
+      printf "Generates a repo prepped for usage with homeshick\n\n"
+      printf "Usage:\n  homeshick generate [-hq] <castle>...\n"
       ;;
-		list)
-      printf "Lists cloned castles\n"
-      printf "Usage:\n  homeshick list"
+    list)
+      printf "Lists cloned castles\n\n"
+      printf "Usage:\n  homeshick list [-h]\n"
       ;;
-		check|updates)
-      printf "Checks if a castle has been updated on the remote\n"
-      printf "Usage:\n  homeshick $1 [CASTLE..]"
+    check|updates)
+      printf "Checks if a castle has been updated on the remote\n\n"
+      printf "Usage:\n  homeshick check [-hq] [<castle>...]"
       ;;
     refresh)
-      printf "Checks if a castle has not been pulled in DAYS days.\n"
+      printf "Checks if a castle has not been pulled in DAYS days.\n\n"
       printf "The default is one week.\n"
-      printf "Usage:\n  homeshick refresh [DAYS] [CASTLE..]"
+      printf "Usage:\n  homeshick refresh [-bhq] [--days=<days>] [<castle>...]\n"
       ;;
-		pull)
-      printf "Updates a castle. Also recurse into submodules.\n"
-      printf "Usage:\n  homeshick pull [CASTLE..]"
+    pull)
+      printf "Updates a castle. Also recurse into submodules.\n\n"
+      printf "Usage:\n  homeshick pull [-bhq] [<castle>...]\n"
       ;;
-		link|symlink)
-      printf "Symlinks all dotfiles from a castle\n"
-      printf "Usage:\n  homeshick $1 [CASTLE..]"
+    link|symlink)
+      printf "Symlinks all dotfiles from a castle\n\n"
+      printf "Usage:\n  homeshick link [(-f|-s)] [-bhq] [<castle>...]\n"
       ;;
-		track)
-      printf "Adds a file to a castle.\n"
+    track)
+      printf "Adds a file to a castle. "
       printf "This moves the file into the castle and creates a symlink in its place.\n"
-      printf "Usage:\n  homeshick track CASTLE FILE.."
-      ;;
-		help)
-      printf "Shows usage of a task\n"
-      printf "Usage:\n  homeshick help [TASK]"
+      printf "Usage:\n  homeshick track [-fbhq] <castle> <file>...\n"
       ;;
 		*)    help  ;;
 		esac
